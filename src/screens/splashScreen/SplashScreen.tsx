258a568111945da5e7ebb79771bff9bc9709566b
Mohammed Sithik <mohammed@divum.in>
2025-09-07 18:10:20 +0530

    Added Splachscreen

--- src/screens/splashScreen/SplashScreen.tsx
+++ src/screens/splashScreen/SplashScreen.tsx
@@ -2,8 +2,23 @@ import { Text, View } from 'react-native';
 import Subtract from '../../assets/Icons/Subtract.svg';
 import styles from './Styles';
 import { Title } from '../../constants/Strings';
+import { useEffect } from 'react';
+import { RootStackScreenProps } from '../../navigation/Routes';
+import { ScreenName } from '../../navigation/ScreenName';
 
-const SplashScreen = () => {
+const SplashScreen = ({ navigation }: RootStackScreenProps<'SplashScreen'>) => {
+  const navigetToWalkthrough = () => {
+    navigation.navigate(ScreenName.WALKTHROUGH);
+  };
+
+  const navigateToLogin = () => {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigetToWalkthrough();
+    }, 1000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
--- src/screens/walkthrough/Walkthrough.tsx
+++ src/screens/walkthrough/Walkthrough.tsx
@@ -1,10 +1,39 @@
 import { Text, View } from 'react-native';
+import {
+  Walkthrough1,
+  Walkthrough2,
+  Walkthrough3,
+} from '../../assets/Images/Images';
 
 const Walkthrough = () => {
+  const WalkthroughData = [
+    {
+      id: 1,
+      title: 'Discover Verified Experts Instantly',
+      description:
+        'CA, Lawyers, Doctors, Architects — all in one place.Find trusted professionals in Finance, Legal, Healthcare & Infrastructure.',
+      image: Walkthrough1,
+    },
+    {
+      id: 2,
+      title: 'Book Appointments, On Your Time',
+      description:
+        'Check real-time availability. Schedule video calls or in-person meetings with just a few taps.',
+      image: Walkthrough2,
+    },
+    {
+      id: 3,
+      title: 'Pay Securely. Get Instant Invoices.',
+      description:
+        'Integrated wallet, UPI, card payments & auto-generated bills — fully secure and compliant.',
+      image: Walkthrough3,
+    },
+  ];
+
   return (
-    <View>
-      <Text>Walkthrough</Text>
-    </View>
+    <View
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    ></View>
   );
 };
 export default Walkthrough;
