e39808442c71307688572bab60e683be3f387ab5
Mohammed Sithik <mohammed@divum.in>
2025-09-07 17:44:38 +0530

    Added SVG icon render setup

--- src/screens/splashScreen/SplashScreen.tsx
+++ src/screens/splashScreen/SplashScreen.tsx
@@ -1,11 +1,15 @@
 import { Text, View } from 'react-native';
 import FontStyles from '../../assets/stylesheets/FontStyles';
 
+import Subtract from '../../assets/Icons/Subtract.svg';
+
 const SplashScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ ...FontStyles.interSB14 }}>SplashScreen</Text>
       <Text style={{ ...FontStyles.interBold14 }}></Text>
+
+      <Subtract />
     </View>
   );
 };
